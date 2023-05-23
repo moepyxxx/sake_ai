d239d5f2c0dd54961c9360bb683ccb6058d7bd0e
moeko-iwai <moepyxxxxxx@gmail.com>
2023-05-23 20:41:49 +0900

    jsonファイルから読み出すようにした

--- ai/main.py
+++ ai/main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
+from langchain.prompts import load_prompt
 from langchain.chains import LLMChain
 
 load_dotenv()
@@ -10,10 +11,13 @@ key = os.getenv('OPENAI_API_KEY')
 args = sys.argv
 
 llm = OpenAI(openai_api_key=key)
-prompt = PromptTemplate(
-    input_variables=["product"],
-    template="What is a good name for a company that makes {product}?",
-)
+prompt = load_prompt("prompt.json")
 
+# prompt = PromptTemplate(
+#     input_variables=["adjective", "content"],
+#     template="{adjective}{content}といえば？"
+# )
+
+# print(prompt.format(adjective="大きい", content="犬"))
 chain = LLMChain(llm=llm, prompt=prompt)
-print(chain.run(args[1]))
+print(chain.run(adjective="大きい", content="犬"))
