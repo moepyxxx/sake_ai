18d3d04bc82b253dec71f75c16cfe7cf6a75b015
moeko-iwai <moepyxxxxxx@gmail.com>
2023-05-25 07:17:04 +0900

    overall_chainができるようにした

--- ai/main.py
+++ ai/main.py
@@ -5,14 +5,22 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.prompts import load_prompt
 from langchain.chains import LLMChain
+from langchain.chains import SimpleSequentialChain
 
 load_dotenv()
 key = os.getenv('OPENAI_API_KEY')
 args = sys.argv
 
 llm = OpenAI(openai_api_key=key)
-prompt = load_prompt("prompt.json")
+recommend_prompt = load_prompt("recommend_prompt.json")
+analytics_prompt = load_prompt("analytics_prompt.json")
 
-chain = LLMChain(llm=llm, prompt=prompt)
-result = chain.run(hoshi_go="雪の茅舎,伯楽星,冩楽", hoshi_yon="ささのつゆ", hoshi_san="久保田,田酒", hoshi_ni="作,ロ万", hoshi_ichi="上善如水")
+recommend_chain = LLMChain(llm=llm, prompt=recommend_prompt)
+analytics_chain = LLMChain(llm=llm, prompt=analytics_prompt)
+overall_chain = SimpleSequentialChain(
+    chains=[recommend_chain, analytics_chain], 
+    verbose=True
+)
+
+result = overall_chain.run(hoshi_go="雪の茅舎,伯楽星,冩楽", hoshi_yon="ささのつゆ", hoshi_san="久保田,田酒", hoshi_ni="作,ロ万", hoshi_ichi="上善如水")
 print(result)
--- api/main.py
+++ api/main.py
@@ -7,13 +7,15 @@ from langchain.prompts import PromptTemplate
 from langchain.prompts import load_prompt
 from langchain.chains import LLMChain
 from pydantic import BaseModel
+from langchain.chains import SequentialChain
 
 load_dotenv()
 key = os.getenv('OPENAI_API_KEY')
 app = FastAPI()
 
 llm = OpenAI(model_name="text-davinci-003",openai_api_key=key, max_tokens=-1)
-prompt = load_prompt("prompt.json")
+recommend_prompt = load_prompt("recommend_prompt.json")
+analytics_prompt = load_prompt("analytics_prompt.json")
 
 class SakeReview(BaseModel):
     hoshi_go: str
@@ -24,6 +26,13 @@ class SakeReview(BaseModel):
 
 @app.post("/")
 async def answer(review: SakeReview):
-    chain = LLMChain(llm=llm, prompt=prompt)
-    result = chain.run(hoshi_go=review.hoshi_go, hoshi_yon=review.hoshi_yon, hoshi_san=review.hoshi_san, hoshi_ni=review.hoshi_ni, hoshi_ichi=review.hoshi_ichi)
+    recommend_chain = LLMChain(llm=llm, prompt=recommend_prompt, output_key="recommend")
+    analytics_chain = LLMChain(llm=llm, prompt=analytics_prompt, output_key="analytics")
+
+    overall_chain = SequentialChain(
+        chains=[recommend_chain, analytics_chain],
+        input_variables=["hoshi_go", "hoshi_yon", "hoshi_san", "hoshi_ni", "hoshi_ichi"],
+        verbose=True
+    )
+    result = overall_chain.run({"hoshi_go":review.hoshi_go, "hoshi_yon":review.hoshi_yon, "hoshi_san":review.hoshi_san, "hoshi_ni":review.hoshi_ni, "hoshi_ichi":review.hoshi_ichi})
     return {"result": result}
