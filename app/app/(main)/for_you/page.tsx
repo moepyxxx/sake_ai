4ed99744a854f154f3827b8f4aea98d762248530
moeko-iwai <moepyxxxxxx@gmail.com>
2023-06-10 14:29:24 +0900

    ちょっと文言をかえておいた（へんだったから）

--- app/app/(main)/for_you/page.tsx
+++ app/app/(main)/for_you/page.tsx
@@ -5,7 +5,10 @@ export default async function ForYouPage() {
   const { recommend, analytics } = await fetchRecommendSakeAction();
   return (
     <>
-      <Title title="for you" description="あなたのsakeをaiが解説するよ" />
+      <Title
+        title="for you"
+        description="あなたのsakeのこのみをaiが解説するよ"
+      />
       <div className="p-4 text-white bg-cyan-600 rounded-lg mb-4">
         <div className="text-center pb-3">
           <p className="text-lg">あなたに次におすすめのsake</p>
